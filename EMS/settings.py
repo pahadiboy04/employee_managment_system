f9805996372a8527b45045d9570b501c166bbecc
Anurag gusain <123312935+pahadiboy04@users.noreply.github.com>
2025-06-22 01:59:57 +0530

    Update settings.py

--- EMS/settings.py
+++ EMS/settings.py
@@ -123,4 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'employee', 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
